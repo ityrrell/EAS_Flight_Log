abaae43146737092d876477fe2d29531d852c05d
Ian Tyrrell <ian.g.tyrrell@outlook.com>
2019-02-28 22:00:02 +0200

    New Update (28/02/2019) / 21:59

--- EAS_Flight_Log/EAS_Flight_Log/EAS_Main.vb
+++ EAS_Flight_Log/EAS_Flight_Log/EAS_Main.vb
@@ -1,6 +1,7 @@
 ﻿Public Class EAS_Main
     Private Sub NewFlightToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewFlightToolStripMenuItem.Click
-
+        Dim FS_Open As Integer
+        FS_Open = Connect_FSUIPC()
     End Sub
 
     Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
