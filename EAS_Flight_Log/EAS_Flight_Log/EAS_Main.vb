a89848ce7df69e871644276954c8031cc1c5c056
Ian Tyrrell <ian.g.tyrrell@outlook.com>
2019-02-28 21:18:11 +0200

    Dail Update

---
+++ EAS_Flight_Log/EAS_Flight_Log/EAS_Main.vb
@@ -0,0 +1,12 @@
+﻿Public Class EAS_Main
+    Private Sub NewFlightToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewFlightToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Application.Exit()
+        End
+    End Sub
+
+
+End Class
---
+++ EAS_Flight_Log/EAS_Flight_Log/Main_Routines.vb
@@ -0,0 +1,6 @@
+﻿Module Main_Routines
+
+    ' Public Variables
+    Public Version As String = "Version: 0.0.0.1"
+
+End Module
